000a8b47463377ad7051b377ee59df4eafbb8016
Konstantin.Parhomchuk <rjcnz542@mail.ru>
2025-03-21 22:28:10 +0300

    merge succesfull

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ public class Main {
         printColor();
         compareNumbers();
         method();
-        method1();
-        method2();
-
+        method8();
 
 
 
     }
 
+    //1
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
 
 
     }
 
+    //2
     public static void checkSumSign() {
         int a = 1;
         int b = 1;
@@ -35,8 +35,9 @@ public class Main {
         }
     }
 
+    //3
     public static void printColor() {
-       int value = 0;
+        int value = 0;
 
         if (value < 0) {
             System.out.println("Red");
@@ -45,68 +46,79 @@ public class Main {
             System.out.println("Yellow");
         }
 
-            if (value > 101) {
+        if (value > 101) {
             System.out.println("Green");
         }
     }
-    public static void compareNumbers(){
-        int a=1;
-        int b=2;
-        if(a>=b){
+
+    //4
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 2;
+        if (a >= b) {
             System.out.println("a>=b");
-        }
-        else {
-            if (a<b);
+        } else {
+            if (a < b) ;
             System.out.println("a<b");
         }
     }
-  public static void method(){
-    int a=10;
-    int b=20;
-    int sum=(a+b);
 
-    if(sum>=30){
-        System.out.println(true);
-    }
-    else {
-        if(sum<10);
-        System.out.println(false);
-    }
-  }
-  public static void method1(){
-        int a=0;
+    //5
+    public static void method() {
+        int a = 10;
+        int b = 20;
+        int sum = (a + b);
 
-        if(a<0){
+        if (sum >= 30) {
             System.out.println(true);
-        }
-        else {
-            if(a>=0);
+        } else {
+            if (sum < 10) ;
             System.out.println(false);
         }
-  }
-  public static int method2(){
-        int age=3;
+    }
 
-        switch (age){
+    //6
+    public boolean method1(int a, int b) {
+        a = 0;
+        b = 5;
 
-            case -1:
-                System.out.println(true);
-            break;
 
-            default:
-                System.out.println(false);
+        if (a < 0) {
+            System.out.println(true);
+        } else {
+            if (a >= 0) ;
+            System.out.println(false);
+        }
+        return false;
+    }
 
+    //7
+    public boolean method2(int age) {
+        age = -3;
+
+        if (age < 0) {
+            return true;
 
 
         }
 
+        return false;
+    }
 
-      return age;
-  }
-    static int method3(){
-        return method2();
-  }
+    public static void method8() {
+        String name = "Konstantin";
+        int a=5;
+        int i;
+        for(i=0;i<a;i++){
+            System.out.println(name);
+        }
+
+    }
+
 }
+
+
+
 
 //.
 
